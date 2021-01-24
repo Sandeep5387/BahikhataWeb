569c776efe135877ccd6a8484e02632f9d8b6f8b
Saurabh Chauhan <saurabh.chauhan@ATMECS.CORP>
2021-01-24 16:06:50 +0530

    Updated product details form array

--- src/app/helper/http-interceptor.ts
+++ src/app/helper/http-interceptor.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpResponse, HttpRequest, HttpHandler, HttpHeaders } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpResponse,
+  HttpRequest,
+  HttpHandler,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class ApplicationHttpInterceptor implements HttpInterceptor {
-  intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    httpRequest: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     const authReq = httpRequest.clone({
-        headers: new HttpHeaders({
-          'Content-Type':  'application/json',
-          'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjkxNzkxNDk4OTciLCJuYW1laWQiOiI2MDA0MmFmZDQxMWQ5ZWMzYWE3NzU1MjYiLCJuYmYiOjE2MTEyMzIzODksImV4cCI6MTYxMTIzNTk4OSwiaWF0IjoxNjExMjMyMzg5fQ.pcH9MDKjHzC4hFwcu4cYc-v3DVS2DVD0pFFEZjF0mFQ',
-          'device':'1'
-          
-        })
-      });
-      console.log('Intercepted HTTP call', authReq);
-     
-      return next.handle(authReq);
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjkxNzkxNDk4OTciLCJuYW1laWQiOiI2MDA0MmFmZDQxMWQ5ZWMzYWE3NzU1MjYiLCJuYmYiOjE2MTE0ODIxMjYsImV4cCI6MTYxMTQ4NTcyNiwiaWF0IjoxNjExNDgyMTI2fQ.-1O3pekiWTY7u15Rgk9k9_-Hotk77w4f0O-mgXvKAYg',
+        device: '1',
+      }),
+    });
+    console.log('Intercepted HTTP call', authReq);
+
+    return next.handle(authReq);
   }
 }
